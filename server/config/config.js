849991230ffcf6149bf1a2e23f3faafe45a8dd63
Phi Huynh <phihuynh@Phis-MacBook-Pro.local>
2020-03-07 10:34:28 -0700

    commit config

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    //database: 'mongodb://localhost/cas-auction',
-    database: 'mongodb+srv://phi:phi123@cluster0-p1wyf.mongodb.net/test?retryWrites=true&w=majority',
+    database: process.env.MONGODB_URI || 'mongodb+srv://phi:phi123@cluster0-p1wyf.mongodb.net/test?retryWrites=true&w=majority',
     port: process.env.PORT || 8080
 }
